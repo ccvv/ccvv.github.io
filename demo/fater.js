2ef7e93e34352603b12fe1a0c56c489cbd0cdbaf
wangxiaowei <297278806@qq.com>
2016-07-22 14:35:36 +0800

    init

--- demo/fater.js
+++ demo/fater.js
@@ -1,6 +1,3 @@
-## web-isSupportH5
-- 判断浏览器是否支持html5
-```
 window.onload = function() {
 	if(window.applicationCache){
 		console.log('support html5'); // 支持
@@ -8,4 +5,3 @@ window.onload = function() {
 		console.log('nonsupport html5'); // 不支持
 	}
 }
-```
